81873e1d15e97ed5931885f8ea2fc8bbeff7130b
MaTT3TV2 <48678574+MaTT3TV2@users.noreply.github.com>
2019-03-18 18:05:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === 'konj) {
 
-       message.reply('pong');
+       message.reply('ponh');
 
        }
 
