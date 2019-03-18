dda4224b9cdecabdf7d02ed2374a25e8092a009e
MaTT3TV2 <48678574+MaTT3TV2@users.noreply.github.com>
2019-03-18 16:08:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,28 +10,10 @@ client.on('ready', () => {
 
 });
 
-bot.on("message", function (message)
-{
-    var input = message.content.toUpperCase();
-
-    if(input === "HELP!")
-    {
-        let adminRoleObject = message.server.roles.get('name', 'admins');
-        bot.sendMessage(message, `${adminRoleObject} are on the way!`);
-    }
-    if(input === "l")
-    {
-        bot.sendMessage(message, "@admin");
-    }
-    if(input === "WHO MADE SHADOWBOT?")
-
-    {
-        let ownerUserObject = message.server.members.get('name', 'Techmo3014');
-        bot.sendMessage(message, `${ownerUserObject} made me.`);
-    }
-
+client.on('message', msg => {
+  if (msg.content === '!Aiuto& msg.channel.id == '556786013264150530') {
+    msg.reply('Ha bisogno di aiuto nel canale assistenza @admin');
+  }
 });
-
-bot.loginWithToken("NTU2Nzc1NTk3NDY5MDczNDE5.D3FE2Q.6-JrnjNTsRNH1F_GSZL2UUEfEkQ");
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
