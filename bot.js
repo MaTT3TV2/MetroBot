be78efb7789ab712a0965866fd9ce18a7ba90391
MaTT3TV2 <48678574+MaTT3TV2@users.noreply.github.com>
2019-03-18 13:17:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'ping') {
+    if (message.content === '!Aiuto') {
 
-       message.reply('pong');
+       message.reply('L'utente {user} ha bisogno di aiuto nel canale assistenza @SupportoUtenti')
 
        }
 
