02724c5457d92b5112c344c4fa6dc81f3a70196c
MaTT3TV2 <48678574+MaTT3TV2@users.noreply.github.com>
2019-03-19 21:43:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,19 @@
-const Discord = require('discord.js');
+/*
+ * Discord Bot Builder Bot
+ * Version 1.2.0
+ * Robert Borghese
+ */
 
-const client = new Discord.Client();
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
- 
+if(!process.send) {
 
-client.on('ready', () => {
+Files.initStandalone();
 
-    console.log('I am ready!');
+} else {
 
+process.on('message', function(content) {
+	Files.initBotTest(content);
 });
 
- 
-
-client.on('!Aiuto', client => {
-    client.channels.get('556786013264150530').send('Ha bisogno di aiuto nel canale assistenza @admin')
-})
-
-
-
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+}
